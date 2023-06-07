a1a9516942291e2694813661187ccb5a7ac3c754
FannyVA <fannyviveros82@gmail.com>
2023-06-07 12:11:36 -0600

    Practica

--- Practica 8/index.js
+++ Practica 8/index.js
@@ -1,38 +1,50 @@
 const searchInput = document.getElementById('search');
-const results = document.getElementById('results');
 const searchButton = document.getElementById('button');
+const results = document.getElementById('results');
 
-searchButton.addEventListener('click', searGIFs);
+searchButton.addEventListener('click', searchGIFs);
 
-function searGIFs() {
-    const search = searchInput.Value;
-    return getGiphyResults(url)
-        .then(function (gifs) {
-            console.log(gifs)
-            gifs.forEach(function (gif) {
-                const img = document.createElement('img')
-                img.src = gif.imagen.fixed_height.url;
-                img.alt = gif.title
-                results.appendChild(img)
-            });
+function searchGIFs() {
+    const search = searchInput.value;
 
-        })
+    if (search) {
+        const formattedSearch = formatSearchString(search);
+        const url = buildUrl(formattedSearch);
+
+        return getGiphyResults(url)
+            .then(function (gifs) {
+                console.log(gifs)
+                gifs.forEach(function (gif) {
+                    const img = document.createElement('img');
+                    img.src = gif.images.fixed_height.url;
+                    img.alt = gif.title;
+
+                    results.appendChild(img)
+                })
+            })
+    }
 }
+
+function formatSearchString(search) {
+    return search.replace(/ /g, '+');
+}
+
 function buildUrl(search) {
-    const API_KEY = 'neXoghkE4n1e83PFswNfWPcyH5Cos3Mz';
-    const Baseurl = 'api.giphy.com/v1/gifs/search';
-    return `${baseUrl}?q=${search}&api_key=${API_KEY}&limit=9`;
+    const API_KEY = 'neXoghkE4n1e83PFswNfWPcyH5Cos3Mz'; // llave de GIPHY
+    const baseUrl = 'http://api.giphy.com/v1/gifs/search';
 
+    return `${baseUrl}?q=${search}&api_key=${API_KEY}&limit=9`;
 }
+
 function getGiphyResults(url) {
-return fetch(url)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            return data.data
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
 }
